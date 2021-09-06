2976899ed3f1c1d41cc60cad44a167ad61df26b1
jschiarizzi <jschiarizzi@gwu.edu>
2021-09-06 17:11:51 -0400

    basic library setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # chess-achievements
 calculate achievements for games of chess
+
+
+## setup
+Run `npm i` to install dependencies (there arent any)
+
+Put code in index.js
---
+++ test.js
@@ -0,0 +1,9 @@
+const achievementLibrary = require("./index.js");
+//import index.js which is the main library
+
+console.log(achievementLibrary);
+
+var result = achievementLibrary.achievements();
+//call functions on it
+
+//Do other tests here.
