063ba4d39c0960fc61b77c2bb171083eed5362d8
patrick <blockchainbuddha@gmail.com>
2021-11-10 16:37:24 -0500

    update canary

--- tasks/npm-publish.sh
+++ tasks/npm-publish.sh
@@ -2,9 +2,14 @@
 
 D="$(dirname "$0")"
 
-TAG="$1"
+PACKAGE_DIR="$1"
+TAG="$2"
 shift 2
 
-echo "Publishing package @${TAG} to NPMJS registry"
+echo "Publishing ${PACKAGE_DIR} @${TAG} to NPMJS registry"
 $D/npmrc-use-npmjs.sh > .npmrc
-npm publish --tag ${TAG} ./ "$@"
+npm publish --tag ${TAG} ${PACKAGE_DIR} "$@"
+
+echo "Publishing ${PACKAGE_DIR} @${TAG} to Github Packages"
+$D/npmrc-use-github.sh > .npmrc
+npm publish --tag ${TAG} ${PACKAGE_DIR} "$@"
