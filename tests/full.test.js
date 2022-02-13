67d22f595de7f633d447a6f851cc82f2d34c84b4
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-02-13 15:43:26 -0700

    Updates test file to throw errors instead of console logging

--- tests/full.test.js
+++ tests/full.test.js
@@ -11,6 +11,29 @@ let errorGames = [];
 let successfulGames = [];
 let achievementsToCheck = {};
 
+function checkAchievementCount(expected, calculated) {
+  if (expected.length !== calculated.length) {
+    throw 'ERROR: incorrect number of achievements';
+  }
+}
+
+function achievementChecker(gameNo, expected, calculated) {
+  expected.forEach(achievement => {
+    if (!calculated.includes(achievement)) {
+      gamesToCheck.push(gameNo);
+      throw `MISSING ACHIEVEMENT: ${achievement}`;
+    }
+  });
+}
+
+function errorFlagger(flag, gameNo, missing, calculated) {
+  if (flag) {
+    throw `Error detected while testing Game #${gameNo}`;
+  } else {
+    successfulGames.push(gameNo);
+  }
+}
+
 // Tests all games in examplePgns.js
 examplePgns.forEach(game => {
   let gameNo = game.gameNo;
@@ -30,35 +53,75 @@ examplePgns.forEach(game => {
       calculatedAchievements.push(achievement['name']);
     });
 
-    if (expectedAchievements.length !== calculatedAchievements.length) {
-      gamesToCheck.push(gameNo);
-      console.log('ERROR: incorrect number of achievements');
+    // if (expectedAchievements.length !== calculatedAchievements.length) {
+    //   gamesToCheck.push(gameNo);
+    //   console.log('ERROR: incorrect number of achievements');
+    //   errorFlag = true;
+    // }
+
+    // Throw error when achievement length doesn't match
+    try {
+      checkAchievementCount(
+        expectedAchievements,
+        calculatedAchievements,
+        errorFlag
+      );
+    } catch (e) {
       errorFlag = true;
+      gamesToCheck.push(gameNo);
+      console.log(e);
     }
 
-    expectedAchievements.forEach(achievement => {
-      if (!calculatedAchievements.includes(achievement)) {
-        gamesToCheck.push(gameNo);
-        console.log('MISSING ACHIEVEMENT: ' + achievement);
-        if (achievementsToCheck.hasOwnProperty(achievement)) {
-          achievementsToCheck[achievement] += 1;
-        } else {
-          achievementsToCheck[achievement] = 1;
-        }
-        missingAchievements.push(achievement);
-        errorFlag = true;
-      }
-    });
+    // expectedAchievements.forEach(achievement => {
+    //   if (!calculatedAchievements.includes(achievement)) {
+    //     gamesToCheck.push(gameNo);
+    //     console.log('MISSING ACHIEVEMENT: ' + achievement);
+    //     if (achievementsToCheck.hasOwnProperty(achievement)) {
+    //       achievementsToCheck[achievement] += 1;
+    //     } else {
+    //       achievementsToCheck[achievement] = 1;
+    //     }
+    //     missingAchievements.push(achievement);
+    //     errorFlag = true;
+    //   }
+    // });
 
-    if (errorFlag) {
-      console.log(`ERROR: ISSUE DETECTED WHILE TESTING Pgn #${gameNo}`);
-      console.log('Missing Achievements:');
-      console.log(missingAchievements);
-      console.log('Calculated Achievements:');
-      console.log(calculatedAchievements);
-    } else {
-      successfulGames.push(gameNo);
+    // Throws error if a specific achievement is missing
+    try {
+      achievementChecker(
+        gameNo,
+        expectedAchievements,
+        calculatedAchievements,
+        errorFlag
+      );
+    } catch (e) {
+      errorFlag = true;
+      gamesToCheck.push(gameNo);
+      console.log(e);
     }
+
+    // Throws error if error flag is true
+    try {
+      errorFlagger(
+        errorFlag,
+        gameNo,
+        missingAchievements,
+        calculatedAchievements
+      );
+    } catch (e) {
+      errorFlag = true;
+      console.log(e);
+    }
+
+    // if (errorFlag) {
+    //   console.log(`ERROR: ISSUE DETECTED WHILE TESTING Pgn #${gameNo}`);
+    //   console.log('Missing Achievements:');
+    //   console.log(missingAchievements);
+    //   console.log('Calculated Achievements:');
+    //   console.log(calculatedAchievements);
+    // } else {
+    //   successfulGames.push(gameNo);
+    // }
   } catch (e) {
     gamesToCheck.push(gameNo);
     console.log(`Issue while testing pgn #${gameNo}`);
